44b4031c53137a44d1618e7518dea527e6375954
Itz HaSan <146573168+hasan32425@users.noreply.github.com>
2025-01-23 16:48:48 +0600

    Update pair.js

--- scripts/cmds/pair.js
+++ scripts/cmds/pair.js
@@ -54,10 +54,10 @@ module.exports = {
     var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
     var background = [
-      "https://tinyurl.com/27qrfpg8.png",
-      "https://tinyurl.com/22pctvpx.png",
-      "https://tinyurl.com/2cs9yj7g.png",
-      "https://tinyurl.com/2dyhv92j.png"
+      "https://tinyurl.com/27qrfpg8",
+      "https://tinyurl.com/22pctvpx",
+      "https://tinyurl.com/2cs9yj7g",
+      "https://tinyurl.com/2dyhv92j"
     ];
     var rd = background[Math.floor(Math.random() * background.length)];
     let getAvtmot = (
