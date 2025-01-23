8c19571f190e0d2298ef8052f03ad5de1bcd47c5
Itz HaSan <146573168+hasan32425@users.noreply.github.com>
2025-01-23 16:40:22 +0600

    Update pair2.js

--- scripts/cmds/pair2.js
+++ scripts/cmds/pair2.js
@@ -60,7 +60,7 @@ const { loadImage, createCanvas } = require("canvas");
  var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
  var background = [
- "https://i.ibb.co/RBRLmRt/Pics-Art-05-14-10-47-00.jpg"
+ "https://tinyurl.com/23apf67q.jpg"
  ];
 
  let getAvtmot = (
